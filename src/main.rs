7e9be1331912df8d5ffff0b65b357e2c274ce036
pavitra-infocusp <pavitra@infocusp.com>
2023-11-24 13:03:04 +0530

    stage 1

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,6 @@ use std::env;
 use std::fs;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    Uncomment this block to pass the first stage
     let args: Vec<String> = env::args().collect();
     if args[1] == "init" {
         fs::create_dir(".git").unwrap();
