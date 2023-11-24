7fd004f3861e07d08ffb122ebd17de862a5abc6f
pavitra-infocusp <pavitra@infocusp.com>
2023-11-24 14:52:39 +0530

    stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,47 @@
-#[allow(unused_imports)]
-use std::env;
-#[allow(unused_imports)]
 use std::fs;
+use clap::{Parser, Subcommand};
+use flate2::read::ZlibDecoder;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Init,
+
+    CatFile {
+        #[arg(short)]
+        print: bool,
+
+        #[clap(required = true)]
+        blob_sha: String,
+    }
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-        println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Init => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
+            println!("Initialized git directory");
+        }
+
+        Commands::CatFile { print, blob_sha } => {
+            let object = fs::read(format!(".git/objects/{}", blob_sha))?;
+
+            let mut z = ZlibDecoder::new(&object[..]);
+            let mut s = String::new();
+            z.read_to_string(&mut s)?;
+
+            print!(s);
+        }
     }
 }
